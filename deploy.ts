1bd544588aa73b4b107c73112a579712192bc2f6
Ryan Wold <wold@afomi.com>
2024-04-03 17:37:03 -0700

    getting error

--- deploy.ts
+++ deploy.ts
@@ -1,7 +1,10 @@
 import { Counter } from './src/contracts/counter'
 import { getDefaultSigner } from './tests/utils/txHelper'
+import {
+  MethodCallOptions
+} from 'scrypt-ts'
 
-async function main() {
+async function deploy() {
     await Counter.loadArtifact()
 
     const count = BigInt(2)
@@ -14,4 +17,31 @@ async function main() {
     console.log('Counter contract deployed: ', deployTx.id)
 }
 
-main()
+// deploy()
+
+async function callIncrement(
+    txId: string,
+    atOutputIndex = 0
+): Promise<string> {
+    // Fetch TX via provider and reconstruct contract instance.
+    const signer = getDefaultSigner()
+    const tx = await signer.connectedProvider.getTransaction(txId)
+    const instance = Counter.fromTx(tx, atOutputIndex)
+
+    await instance.connect(signer)
+
+    const nextInstance = instance.next()
+    nextInstance.increment()
+
+    const { tx: callTx } = await instance.methods.incrementOnChain({
+        next: {
+            instance: nextInstance,
+            balance: instance.balance,
+        },
+    } as MethodCallOptions<Counter>)
+    console.log(`Counter incrementOnChain called: ${callTx.id}, the count now is: ${nextInstance.count}`)
+    return callTx.id
+}
+
+// https://test.whatsonchain.com/tx/ca4c16ee87bf8d273710c4e7808f7c3893bfcdbfe9b3725a4f3c3f06aa8898dc
+callIncrement("ca4c16ee87bf8d273710c4e7808f7c3893bfcdbfe9b3725a4f3c3f06aa8898dc")
