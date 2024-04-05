5a9f256034d3b6d38992dd9b451d0b07236aae6e
Ryan Wold <wold@afomi.com>
2024-04-04 21:59:17 -0700

    add a third method to deployAndCall()

--- deploy.ts
+++ deploy.ts
@@ -1,9 +1,7 @@
 import { Counter } from './src/contracts/counter'
 import artifact from './artifacts/counter.json'
 import { getDefaultSigner } from './tests/utils/txHelper'
-import {
-  MethodCallOptions
-} from 'scrypt-ts'
+import { MethodCallOptions } from 'scrypt-ts'
 
 async function deploy() {
     await Counter.loadArtifact()
@@ -14,20 +12,18 @@ async function deploy() {
     await instance.connect(getDefaultSigner())
 
     // deploy the contract and lock up 42 satoshis in it
-    const deployTx = await instance.deploy(42)
+    const deployTx = await instance.deploy(1)
     console.log('Counter contract deployed: ', deployTx.id)
+    return deployTx.id
 }
 
 // deploy()
 
-async function callIncrement(
-    txId: string,
-    atOutputIndex = 0
-): Promise<string> {
+async function callIncrement(txId: string, atOutputIndex = 0): Promise<string> {
     await Counter.loadArtifact(artifact)
     // Fetch TX via provider and reconstruct contract instance.
     const signer = getDefaultSigner()
-    await signer.connect();
+    await signer.connect()
 
     const tx = await signer.connectedProvider.getTransaction(txId)
     const instance = Counter.fromTx(tx, atOutputIndex)
@@ -43,9 +39,19 @@ async function callIncrement(
             balance: instance.balance,
         },
     } as MethodCallOptions<Counter>)
-    console.log(`Counter incrementOnChain called: ${callTx.id}, the count now is: ${nextInstance.count}`)
+    console.log(
+        `Counter incrementOnChain called: ${callTx.id}, the count now is: ${nextInstance.count}`
+    )
     return callTx.id
 }
 
 // https://test.whatsonchain.com/tx/ca4c16ee87bf8d273710c4e7808f7c3893bfcdbfe9b3725a4f3c3f06aa8898dc
-callIncrement("ca4c16ee87bf8d273710c4e7808f7c3893bfcdbfe9b3725a4f3c3f06aa8898dc")
+// callIncrement("ca4c16ee87bf8d273710c4e7808f7c3893bfcdbfe9b3725a4f3c3f06aa8898dc")
+// callIncrement("c8f1162f099b240d8952b24c36b7ac152585ede78c56f9555949ece1bf8e0722")
+
+async function deployAndCall() {
+    const txId = await deploy()
+    callIncrement(txId)
+}
+
+deployAndCall()
